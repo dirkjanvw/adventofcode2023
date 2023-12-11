275b36fad6d36d74ce8fe44426d7abc3a0c88b6f
worku005 <dirk-jan.vanworkum@wur.nl>
2023-12-11 21:59:32 +0100

    finish solution 1 day 11

---
+++ day11/solution1/src/Main.java
@@ -0,0 +1,14 @@
+import java.nio.file.Paths;
+
+public class Main {
+    public static void main(String[] args) {
+        // read sky data from file
+        Sky sky = new Sky(Paths.get(args[0]));
+
+        // expand galaxy
+        sky.expand();
+
+        // calculate pair-wise distance between galaxies and sum
+        System.out.println(sky.sumDistances());
+    }
+}
